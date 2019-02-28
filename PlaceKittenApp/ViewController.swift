91ec5ee5e5d218615244de169c3184eb60897776
MAC Consultant <consultant@MacBook-Pro-10.routerlogin.net>
2019-02-28 18:31:39 -0500

    First Commit. Created basic layout. pressing go displays a width x height place holder image from Kitteplace

--- PlaceKittenApp/ViewController.swift
+++ PlaceKittenApp/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var widthTextField: UITextField!
+    @IBOutlet weak var heightTextField: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func onButtonPush(_ sender: Any) {
+        
+        let width = Int(widthTextField.text ?? "20")
+        let height = Int(heightTextField.text ?? "20")
+        
+        guard let w = width else{return}
+        guard let h = height else{return}
+        
+        let addr = "https://placekitten.com/" + String(w) + "/" + String(h)
+        
+        guard let url = URL(string: addr) else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            
+            if let imageData = data{
+                
+                DispatchQueue.main.async {
+                    self.imageView.image = UIImage(data: imageData)
+                }
+            }
+            
+        }.resume()
+        
+        
+        
+        
+        
+    }
 }
 
